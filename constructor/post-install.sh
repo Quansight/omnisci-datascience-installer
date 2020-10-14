8277fd0a9c10b451b164a02676e45392f3d00b3b
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2020-10-13 21:37:32 -0400

    Build locally some deps

--- constructor/post-install.sh
+++ constructor/post-install.sh
@@ -8,10 +8,4 @@ python -m pip install ibis-vega-transform
 jupyter labextension install ibis-vega-transform
 jupyter labextension install @pyviz/jupyterlab_pyviz
 
-
-pip install \
-    git+https://github.com/ibis-project/ibis@10d40305defc7337d217b595c4b6b74e66d153da \
-    git+https://github.com/holoviz/holoviews.git@refs/pull/4517/head \
-    git+https://github.com/holoviz/hvplot.git@refs/pull/507/head
-
 export PATH=$OLD_PATH
