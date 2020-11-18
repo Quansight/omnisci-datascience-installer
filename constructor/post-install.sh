dea51c4ce53e0b4ebf75eb04dd085c2e493ca80c
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2020-11-18 19:50:41 -0400

    pip install intake-omnisci and ibis-vega-transform

--- constructor/post-install.sh
+++ constructor/post-install.sh
@@ -3,6 +3,9 @@
 export OLD_PATH=$PREFIX
 export PATH=$PREFIX/bin:$PATH
 
+pip install intake-omnisci
+pip install ibis-vega-transform
+
 jupyter labextension install ibis-vega-transform
 jupyter labextension install @pyviz/jupyterlab_pyviz
 
