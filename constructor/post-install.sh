64595c00b4f9061304cfd64961029a6bc910b8dd
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2021-01-20 15:13:07 -0400

    Add notebook and installation pathtarget to the conda constructor recipe

--- constructor/post-install.sh
+++ constructor/post-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export OLD_PATH=$PREFIX
-export PATH=$PREFIX/bin:$PATH
+export OLD_PATH=${PREFIX}
+export PATH=${PREFIX}/bin:${PATH}
 
 pip install intake-omnisci
 pip install ibis-vega-transform
@@ -9,4 +9,8 @@ pip install ibis-vega-transform
 jupyter labextension install ibis-vega-transform
 jupyter labextension install @pyviz/jupyterlab_pyviz
 
-export PATH=$OLD_PATH
+export PATH=${OLD_PATH}
+
+USER_NOTEBOOKS_DIR=${HOME}/Documents/omnisci-datascience/notebooks
+mkdir -p ${USER_NOTEBOOKS_DIR}
+ln -s ${PREFIX}/opt/omnisci/notebooks ${USER_NOTEBOOKS_DIR}
