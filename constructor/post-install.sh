5ce8bc3314455b49821c5b824b94f9f771287793
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2021-02-05 16:38:22 -0400

    Fix the target directory (#51)
    * Fix the target directory
    
    * Create symlink for omnisci-examples

--- constructor/post-install.sh
+++ constructor/post-install.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-export OLD_PATH=$PREFIX
-export PATH=$PREFIX/bin:$PATH
+export OLD_PATH=${PREFIX}
+export PATH=${PREFIX}/bin:${PATH}
 
 pip install ibis-vega-transform jaeger-browser
 
 jupyter labextension install ibis-vega-transform
 jupyter labextension install @pyviz/jupyterlab_pyviz
 
-export PATH=$OLD_PATH
+echo "Creating a symlink fo omnisci-examples to user Documents folder ..."
+ln -s ${PREFIX}/opt/omnisci-examples to ${HOME}/Documents/
+
+export PATH=${OLD_PATH}
