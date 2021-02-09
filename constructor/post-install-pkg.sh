187d1048293a6b716bbecfeed27da9fce75a868f
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2021-02-08 22:24:21 -0400

    Change the package name to omnisci-datascience (#53)
    * Change the package name to omnisci-datascience
    
    * Update documentation
    
    * Fixing the post install for pkg installer
    
    * Forcing prefix for sh package as well

--- constructor/post-install-pkg.sh
+++ constructor/post-install-pkg.sh
@@ -6,10 +6,10 @@ export PREFIX=${HOME}/omnisci-datascience
 export OLD_PATH=${PREFIX}
 export PATH=${PREFIX}/bin:${PATH}
 
-pip install ibis-vega-transform jaeger-browser
+${PREFIX}/bin/pip install ibis-vega-transform jaeger-browser
 
-jupyter labextension install ibis-vega-transform
-jupyter labextension install @pyviz/jupyterlab_pyviz
+${PREFIX}/bin/jupyter labextension install ibis-vega-transform
+${PREFIX}/bin/jupyter labextension install @pyviz/jupyterlab_pyviz
 
 echo "Creating a symlink fo omnisci-examples to user Documents folder ..."
 ln -s ${PREFIX}/opt/omnisci-examples to ${HOME}/Documents/
