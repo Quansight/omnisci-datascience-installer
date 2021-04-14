db6e3731a94109711c716303e8d51ee91334786d
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2021-04-14 16:15:24 -0400

    Use the new hvplot and ibis-vega-transform (#59)
    * Use the new hvplot and ibis-vega-transform
    
    * Create folder ${HOME}/Documents if it doesn't exist
    
    * Test jupyterlab extensions
    
    * Change python pinning to 3.8

--- constructor/post-install.sh
+++ constructor/post-install.sh
@@ -3,15 +3,8 @@
 # FORCING PREFIX
 export PREFIX=${HOME}/omnisci-datascience
 
-export OLD_PATH=${PREFIX}
-export PATH=${PREFIX}/bin:${PATH}
-
-${PREFIX}/bin/pip install ibis-vega-transform
-
-${PREFIX}/bin/jupyter labextension install ibis-vega-transform
-${PREFIX}/bin/jupyter labextension install @pyviz/jupyterlab_pyviz
+# Create ${HOME}/Documents/ if it doesn't exist.
+mkdir -p ${HOME}/Documents/
 
 echo "Creating a symlink fo omnisci-examples to user Documents folder ..."
 ln -s ${PREFIX}/opt/omnisci-examples ${HOME}/Documents/
-
-export PATH=${OLD_PATH}
