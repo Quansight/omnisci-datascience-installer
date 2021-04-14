192e246029cb9d85275f922646f38bc2a1ef9e7c
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2021-04-13 21:46:06 -0400

    Use the new hvplot and ibis-vega-transform

--- constructor/post-install.sh
+++ constructor/post-install.sh
@@ -3,15 +3,5 @@
 # FORCING PREFIX
 export PREFIX=${HOME}/omnisci-datascience
 
-export OLD_PATH=${PREFIX}
-export PATH=${PREFIX}/bin:${PATH}
-
-${PREFIX}/bin/pip install ibis-vega-transform
-
-${PREFIX}/bin/jupyter labextension install ibis-vega-transform
-${PREFIX}/bin/jupyter labextension install @pyviz/jupyterlab_pyviz
-
 echo "Creating a symlink fo omnisci-examples to user Documents folder ..."
 ln -s ${PREFIX}/opt/omnisci-examples ${HOME}/Documents/
-
-export PATH=${OLD_PATH}
