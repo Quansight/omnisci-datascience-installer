deafaf9574ed83349860d308a3039bd913957426
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2021-04-14 13:36:31 -0400

    Create folder Documents if it doesn't exist

--- constructor/post-install.sh
+++ constructor/post-install.sh
@@ -3,5 +3,8 @@
 # FORCING PREFIX
 export PREFIX=${HOME}/omnisci-datascience
 
+# Create ${HOME}/Documents/ if it doesn't exist.
+mkdir -p ${HOME}/Documents/
+
 echo "Creating a symlink fo omnisci-examples to user Documents folder ..."
 ln -s ${PREFIX}/opt/omnisci-examples ${HOME}/Documents/
